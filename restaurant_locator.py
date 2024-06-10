808ec67fdc842998e268bdc05a2ed154c0e9c27b
Nicholas Carey <ncarey2400@gmail.com>
2024-06-09 22:56:40 -0400

    added csvs, cleaned up notebook

---
+++ restaurant_locator.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import requests
+import googlemaps
+import os
+
+# Load your CSV
+df = pd.read_csv("C:/Users/ncare/pizza/scraped_data_may24.csv")
+
+# Initialize Google Maps API client
+gmaps = googlemaps.Client(key='AIzaSyBcuiVgenZhGFnf6HdxiGuOjZJzykJeQHM')
+
+# Function to get the address
+def get_address(name, city, state):
+    geocode_result = gmaps.geocode(f"{name}, {city}, {state}")
+    if geocode_result:
+        return geocode_result[0]['formatted_address']
+    return None
+
+# Apply the function to get addresses
+df['Address'] = df.apply(lambda row: get_address(row['Name'], row['City'], row['State']), axis=1)
+
+
+# Save the updated DataFrame to a new CSV
+df.to_csv('all_pizza_restaurants_with_addresses.csv', index=False)
